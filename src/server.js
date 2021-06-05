e6c08662872450737e4e784195e3e2a70c02e801
tai <minhtaile2712@gmail.com>
2021-06-05 10:56:35 +0700

    hello

--- src/server.js
+++ src/server.js
@@ -5,36 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/downstream/command/:id", handleCommand);
-
-app.get("/downstream/provision/:id", (req, res) => {
-  const { id } = req.params;
-
-  let message = {
-    provisionStatus: "SUCCESS",
-  };
-  MQTT.publish(`downstream/provision/${id}`, message);
-
-  return res.send("ok");
-});
+app.post("/down/command/:id", handleCommand);
 
 const MQTT = require("./mqtt");
 
 function handleCommand(req, res) {
-  const { id } = req.params;
-  const { data } = req.body;
+  const { data, entity: id } = req.body;
 
-  console.log(data);
-  MQTT.publish(`downstream/command/${id}`, data);
+  MQTT.publish(`down/command/${id}`, data);
 
   res.send("ok");
 }
-
-app.get("/test/mqtt", (req, res) => {
-  const id = "608a78baaa969877dec4e6f4";
-
-  const message = { a: 1 };
-  MQTT.publish(`upstream/provision/${id}`, message);
-});
 
 module.exports = app;
